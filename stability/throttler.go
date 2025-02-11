c081a58997211a0bf04fb2172459d9af5f6b77e9
yinyanlv <yinyanlv1124@zto.com>
2025-02-11 14:42:31 +0800

    update: 优化

--- stability/throttler.go
+++ stability/throttler.go
@@ -5,11 +5,13 @@ import "time"
 // 节流模式
 type Throttler struct {
 	tokens chan struct{}
+	done   chan struct{}
 }
 
 func NewThrottler(rate int) *Throttler {
 	t := &Throttler{
 		tokens: make(chan struct{}, rate),
+		done:   make(chan struct{}),
 	}
 	for i := 0; i < rate; i++ {
 		t.tokens <- struct{}{}
@@ -22,14 +24,26 @@ func NewThrottler(rate int) *Throttler {
 // 定时检测并放入令牌
 func (t *Throttler) refill(interval time.Duration) {
 	ticker := time.NewTicker(interval)
-	for range ticker.C {
-		for i := 0; i < cap(t.tokens); i++ {
-			select {
-			case t.tokens <- struct{}{}:
-			default:
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-t.done:
+			return
+		case <-ticker.C:
+			for i := 0; i < cap(t.tokens); i++ {
+				select {
+				case t.tokens <- struct{}{}:
+				default:
+				}
 			}
 		}
 	}
+}
+
+func (t *Throttler) Close() {
+	close(t.tokens)
+	close(t.done)
 }
 
 func (t *Throttler) Allow() bool {
